285e90ae481a21b05b0b314b4afd80706ef1ca4e
Vu2n <87818531+Vu2n@users.noreply.github.com>
2024-06-26 20:06:37 +0100

    Add files via upload

---
+++ DC-Sample Mod/DC-SampleMod/Class1.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx.Logging;
+
+public class SampleMod : IMod
+{
+    public string Name => "Sample Mod";
+
+    public void OnLoad() // Function MUST be in the mod, This is what the handler calls first.
+    {
+        ModLoader.Logger.LogInfo("Sample Mod has been loaded!"); // Prints to the BepinEX console (If its enabled)
+        ModStuff();
+    }
+
+    private void ModStuff()
+    {
+        // Do your Mod stuff here etc
+        // I plan to work on an API to make creating items, characters and just anything really to make it easier but for now ill leave it to you smart cookies <3
+    }
+}
